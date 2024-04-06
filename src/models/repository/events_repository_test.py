cb24ca1b34aa4c0f7dded0088f2ccf5e561a53cd
amanda cristina de sousa <amandacdesousa@outlook.com>
2024-04-06 14:56:53 -0300

    improve: Adding try events repository

--- src/models/repository/events_repository_test.py
+++ src/models/repository/events_repository_test.py
@@ -16,8 +16,9 @@ def test_insert_event():
     response = events_repository.insert_event(event)
     print(response)
 
+@pytest.mark.skip(reason="...")
 def test_get_event_by_id():
-    event_id = "meu-uuid1"
+    event_id = "meu-uuid11"
     events_repository = EventsRepository()
     response = events_repository.get_event_by_id(event_id)
     print(response)
