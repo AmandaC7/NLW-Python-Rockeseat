3338e096366414a9f87dc91dd69da7b6b7554119
amanda cristina de sousa <amandacdesousa@outlook.com>
2024-05-11 13:54:42 -0300

    feat: Implementing check_in routes

--- src/models/repository/check_ins_repository_test.py
+++ src/models/repository/check_ins_repository_test.py
@@ -5,8 +5,8 @@ import pytest
 db_connection_handler.connect_to_db()
 
 def test_insert_check_in():
-    attendeeId = "uuid-attendee1"
+    attendee_id = "uuid-attendee1"
 
     check_in_repository = CheckInRepository()
-    response = check_in_repository.insert_check_in(attendeeId)
+    response = check_in_repository.insert_check_in(attendee_id)
     print(response)
